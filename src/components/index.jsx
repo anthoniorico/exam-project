76b315a7342f57d461c9593f41c6903aed437889
Anthony Okechukwu Okafor <anthoniorico@yahoo.com>
2024-06-14 16:54:13 +0100

    Delete src directory

--- src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export {default as HomeNav} from './HomeNav'
-export {default as Error404} from './Error404'
-export {default as ErrorBoundary} from './ErrorBoundary'
-export {default as Pagination} from './Pagination'
-export {default as ServicesNav} from './ServicesNav'
--- src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export {default as GithubList} from './GithubList'
-export {default as GithubPage} from './GithubPage'
-export {default as HomePage} from './HomePage'
-export {default as Services} from './Services'
